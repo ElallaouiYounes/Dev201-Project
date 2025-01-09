8228ea0c78bb545ad62a68eefb0031cd51473a99
Yassine Bouguarne <yassine.bougarne01@gmail.com>
2025-01-09 16:44:30 +0100

    add top sales component

--- src/components/TopSales.jsx
+++ src/components/TopSales.jsx
@@ -5,7 +5,7 @@ function TopSales({ productData }) {
     <div className="w-full flex flex-col justify-center">
       <div className="text-center font-bold text-4xl my-6">TOP SALES</div>
       <div className="w-full overflow-x-auto scrollbar-hide scroll-smooth">
-        <div className="flex gap-3 my-6 min-w-max px-4">
+        <div className="flex gap-8 my-6 min-w-max px-4">
           {productData.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}

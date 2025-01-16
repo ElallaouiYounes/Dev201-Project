d93fd3ff4cb1b4fbc48b11db2c5cc99b76a39c54
ElallaouiYounes <younesselallaoui444@gmail.com>
2025-01-16 18:53:48 +0100

    i just added hover effect to card component

--- src/components/HomeComponents/ProductCard.jsx
+++ src/components/HomeComponents/ProductCard.jsx
@@ -29,7 +29,7 @@ const Card = ({ product }) => {
 
   const dispalName = name.length >= 24 ? name.slice(0, 22) + "..." : name;
   return (
-    <div className="flex flex-col justify-center items-center border-2 rounded-sm w-[230px] h-[300px] m-4 py-2">
+    <div className="flex flex-col justify-center items-center border-2 rounded-sm w-[230px] h-[300px] m-4 py-2 cursor-pointer transform hover:scale-105 transition duration-300 ease-in-out">
       <div className="w-[90%] h-[70%] bg-contain bg-no-repeat bg-center" style={{ backgroundImage: `url(${image})` }}>
       </div>
       <div className="flex flex-col justify-center w-[90%] h-[20%] font-semibold">

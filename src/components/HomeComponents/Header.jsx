50fae3b5c8c44109d5438281e87d1eeac419af43
ElallaouiYounes <younesselallaoui444@gmail.com>
2025-01-20 14:50:04 +0100

    new updaate

--- src/components/HomeComponents/Header.jsx
+++ src/components/HomeComponents/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   // #40adb5
   return (
     <div className="flex justify-center items-center h-8 bg-gray-800 text-white">
-      <p className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl text-center font-anton">
+      <p className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl text-center">
         The Biggest Sale of the Year is Here! 🚨 Black Friday is LIVE! Don’t
         miss out—Shop Now & Save Big!
       </p>
